65a23a00c3dd353aace16919cc227221c5acd3d8
Ketlyn N. <ketlyn.jesus.santos@escola.pr.gov.br>
2026-06-15 20:15:33 +0000

    up

--- ex04.js
+++
@@ -1,6 +0,0 @@
-const executarOperacao = (a, b, operacao) => console.log(operacao(a, b));
-
-executarOperacao(10, 5, (a, b) => a + b);
-executarOperacao(10, 5, (a, b) => a - b);
-executarOperacao(10, 5, (a, b) => a * b);
-executarOperacao(10, 5, (a, b) => a / b);
